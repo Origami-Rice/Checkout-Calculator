b666b1cbe05f4318f64b88722e88176521ad36d8
william.chu001@gmail.com <william.chu001@gmail.com>
2020-10-04 21:25:44 -0400

    Update main.py

--- mobile_app/backend/main.py
+++ mobile_app/backend/main.py
@@ -1,38 +1,44 @@
-from flask import Flask, jsonify,request
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 import json
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route('/getCatalogue', methods=['GET'])
 def get_catalogue():
     with open('mobile_app/backend/catalogue.json') as f:
-        catalogue=json.load(f)
+        catalogue = json.load(f)
     f.close
     return jsonify(catalogue)
+
 
 @app.route('/', methods=['GET'])
 def get_hello():
     return 'Hello World!'
 
+
 @app.route('/getTotal/<int:price>/<int:quantities>', methods=['GET'])
 def get_total(price, quantities):
     return str(price * quantities)
 
-@app.route('/submit', methods=['POST','GET'])
+
+@app.route('/submit', methods=['POST', 'GET'])
 def post_order():
     print('it is called!')
-    jsonData=request.get_json()
+    jsonData = request.get_json()
     with open('order.json', 'w') as outfile:
-              json.dump(jsonData, outfile)
+        json.dump(jsonData, outfile)
     return jsonify(jsonData)
+
 
 @app.route('/getOrder', methods=['GET'])
 def get_order():
-    with open('mobile_app/backend/order.json') as f:
-        order=json.load(f)
+    with open('order.json') as f:
+        order = json.load(f)
     f.close
     return jsonify(order)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     app.run(debug=True)
