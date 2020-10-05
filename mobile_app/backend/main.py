744274d90e0b06c9ff585a7a0fcfff503ac71a16
william.chu001@gmail.com <william.chu001@gmail.com>
2020-10-04 20:23:27 -0400

    Update main.py

--- mobile_app/backend/main.py
+++ mobile_app/backend/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify,request
 from flask_cors import CORS
 import json
 app = Flask(__name__)
@@ -6,7 +6,7 @@ CORS(app)
 
 @app.route('/getCatalogue', methods=['GET'])
 def get_catalogue():
-    with open('catalogue.json') as f:
+    with open('mobile_app/backend/catalogue.json') as f:
         catalogue=json.load(f)
     f.close
     return jsonify(catalogue)
@@ -17,8 +17,22 @@ def get_hello():
 
 @app.route('/getTotal/<int:price>/<int:quantities>', methods=['GET'])
 def get_total(price, quantities):
-    print('it is called!')
     return str(price * quantities)
+
+@app.route('/submit', methods=['POST','GET'])
+def post_order():
+    print('it is called!')
+    jsonData=request.get_json()
+    with open('order.json', 'w') as outfile:
+              json.dump(jsonData, outfile)
+    return jsonify(jsonData)
+
+@app.route('/getOrder', methods=['GET'])
+def get_order():
+    with open('order.json') as f:
+        order=json.load(f)
+    f.close
+    return jsonify(order)
 
 if __name__=="__main__":
     app.run(debug=True)
